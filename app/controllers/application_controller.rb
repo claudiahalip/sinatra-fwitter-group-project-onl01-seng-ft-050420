d8b3291011280f3910d767f5fd26a79bab3a65f5
claudia v <claudia4ro@yahoo.com>
2020-06-25 04:05:15 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,37 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do 
+    "Welcome to Fwitter"
+  end 
+  
+  get '/signup' do 
+    erb :'users/signup'
+  end 
+  
+  post '/signup' do
+    if params[:username]=="" || params[:email]=="" || params[:password]==""
+      redirect '/signup'
+    else 
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      redirect '/tweets'
+    end 
+  end
+  
+  
+  get '/login' do 
+    erb :'users/login'
+  end 
+  
+  post '/login' do
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      redirect '/tweets'
+    else
+      redirect 'users/login'
+    end
+    
+  end 
 
 end
